51d36ba7f5dfa5ecc242414d876949947c665ace
526040147@qq.com <526040147@qq.com>
2015-12-21 14:41:20 +0800

    添加注释

--- src/TestJdk7.java
+++ src/TestJdk7.java
@@ -1,23 +1,23 @@
 import java.io.ByteArrayInputStream;
 import javax.xml.stream.XMLInputFactory;
 
+/**
+ * 测试jdk1.7.0.45创建xml最大数
+ * @author thj
+ *
+ */
 public class TestJdk7 {
 
     public static void main(String[] args) throws Exception {
 //    	System.setProperty("entityExpansionLimit", "64002");
-    	System.out.println("--");
+    	System.out.println("--start--");
         String xml = "<?xml version=\"1.0\"?><test></test>";
-
         XMLInputFactory factory = XMLInputFactory.newInstance();
-
         for (int i = 0; i < 64001; i++) {
-
             ByteArrayInputStream stream = new ByteArrayInputStream(xml.getBytes());
-
             factory.createXMLEventReader(stream);
-
         }
-
+        System.out.println("--end--");
     }
 
 }
